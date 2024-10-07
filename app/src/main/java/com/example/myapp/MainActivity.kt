eec5e20b8483ac992c4df48060752a4772ca33c6
Aryan123aryan <aryanjaiswal123123@gmail.com>
2024-10-08 00:09:16 +0530

    Added app navigation method

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -3,13 +3,11 @@ package com.example.myapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.example.myapp.Navigation.AppNav
 
 import com.example.myapp.viewModel.AuthViewModel
-import com.example.myapp.viewModel.ProfileUpdateViewModel
 import com.google.firebase.Firebase
 import com.google.firebase.initialize
 
@@ -22,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             val navController = rememberNavController()
             val viewModel: AuthViewModel = viewModel()
-            val authviewModel: ProfileUpdateViewModel = viewModel()
-            AppNav(navController, viewModel, authviewModel, contentResolver )
+            AppNav(navController, viewModel, )
 
         }
     }
